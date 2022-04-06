183973df3dbe1a6a16261aa870c02d96e78058de
mrserg86 <skrylov@blockwit.io>
2022-04-06 13:43:41 +0300

    @GetMapping("/parsingByTag")

--- src/main/java/com/mrserg86/cncmagazine/controllers/MainController.java
+++ src/main/java/com/mrserg86/cncmagazine/controllers/MainController.java
@@ -22,7 +22,7 @@ public class MainController {
         Document document = Jsoup.connect("https://cbr.ru/").get();
         String dirtyUSDRate = String.valueOf(document.getElementsContainingOwnText("₽"));
         String clearUSDRate = dirtyUSDRate.substring(dirtyUSDRate.length()-153,dirtyUSDRate.length()-143);
-        log.info(clearUSDRate);
+        log.info("clearUSDRate : " + clearUSDRate);
         model.addAttribute("clearUSDRate", clearUSDRate);
         return "CNCMagazine";
     }
@@ -31,14 +31,21 @@ public class MainController {
     public String parsingByTag(Model model) throws IOException {
         Document document = Jsoup.connect("https://cbr.ru/").get();
         Elements dirtyUSDRateStep1 = document.getElementsByClass("main-indicator_rate");
-//        log.info("dirtyUSDRate : " + dirtyUSDRate.);
-        for(int i=0; i <= dirtyUSDRateStep1.size(); i++) {
+        Element clearUSDRate = new Element("s");
+        for(int i=0; i < dirtyUSDRateStep1.size(); i++) {
             Element dirtyUSDRateStep2 = dirtyUSDRateStep1.get(i);
             Elements dirtyUSDRateStep3 = dirtyUSDRateStep2.getElementsByClass("_dollar");
             if ( ! dirtyUSDRateStep3.isEmpty()) {
-
+                for(int j =0; j < dirtyUSDRateStep3.size(); j++) {
+                    Element dirtyUSDRateStep4 = dirtyUSDRateStep3.get(j);
+                    Elements dirtyUSDRateStep5 = dirtyUSDRateStep4.getElementsByClass("mono-num");
+                    if (! dirtyUSDRateStep5.isEmpty()) {
+                        clearUSDRate = dirtyUSDRateStep5.get(1);
+                    }
+                }
             }
         }
+        model.addAttribute("clearUSDRate", clearUSDRate);
         return "CNCMagazine";
     }
 
