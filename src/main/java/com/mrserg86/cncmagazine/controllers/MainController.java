f5e5acb47b1d3f3fadec20090a703d25b8b67856
mrserg86 <skrylov@blockwit.io>
2022-04-06 13:48:57 +0300

    @GetMapping("/parsingByTag")

--- src/main/java/com/mrserg86/cncmagazine/controllers/MainController.java
+++ src/main/java/com/mrserg86/cncmagazine/controllers/MainController.java
@@ -31,16 +31,20 @@ public class MainController {
     public String parsingByTag(Model model) throws IOException {
         Document document = Jsoup.connect("https://cbr.ru/").get();
         Elements dirtyUSDRateStep1 = document.getElementsByClass("main-indicator_rate");
+        //1. Найти HTML тэги, которые обозначены классом main-indicator-rate
         Element clearUSDRate = new Element("s");
         for(int i=0; i < dirtyUSDRateStep1.size(); i++) {
             Element dirtyUSDRateStep2 = dirtyUSDRateStep1.get(i);
             Elements dirtyUSDRateStep3 = dirtyUSDRateStep2.getElementsByClass("_dollar");
             if ( ! dirtyUSDRateStep3.isEmpty()) {
+                //2. Из них, выбрать тот !тэг!, внтури которого есть тэг c классом "_dollar"
                 for(int j=0; j < dirtyUSDRateStep3.size(); j++) {
                     Element dirtyUSDRateStep4 = dirtyUSDRateStep3.get(j);
                     Elements dirtyUSDRateStep5 = dirtyUSDRateStep4.getElementsByClass("mono-num");
+                    //3. Из него выбираем содержимое первого тэга с классом mono-num
                     if (! dirtyUSDRateStep5.isEmpty()) {
                         clearUSDRate = dirtyUSDRateStep5.get(1);
+                        //4. Из полученного тэга получаем содержимое
                     }
                 }
             }
