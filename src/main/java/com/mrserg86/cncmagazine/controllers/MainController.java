03ee8452d5062a010138aa1d8c1f3536f8070bce
mrserg86 <skrylov@blockwit.io>
2022-05-18 00:16:18 +0300

    dollar exchange rate protection is only available to authenticated users

--- src/main/java/com/mrserg86/cncmagazine/controllers/MainController.java
+++ src/main/java/com/mrserg86/cncmagazine/controllers/MainController.java
@@ -4,6 +4,8 @@ import com.mrserg86.cncmagazine.services.USDRate;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +24,19 @@ public class MainController {
         return "CNCMagazine";
     }
 
-    @GetMapping("/authenticated")
+    @GetMapping("/USDRate")
     public String au(Model model) throws IOException {
         model.addAttribute("USDRate", usdRate.getClearUSDRate());
-        return "authenticated";
+        Object principal = SecurityContextHolder. getContext(). getAuthentication(). getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails)principal). getUsername();
+            model.addAttribute("Username", username);
+        } else {
+            String username = principal. toString();
+            model.addAttribute("Username", username);
+        }
+
+        return "USDRate";
     }
 
 }
